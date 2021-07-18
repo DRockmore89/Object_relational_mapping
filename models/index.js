fa285b25d5f79e0a46533f74ec15e8b5186b53e3
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-18 15:58:42 -0400

    commit

--- models/index.js
+++ models/index.js
@@ -12,18 +12,23 @@ Product.belongsTo(Category, {
 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'Set Null',
 });
 
+// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
-});
-
+  through: {
+    model: 'ProductTag',
+    foreignKey: 'tag_id',
+  },
+}),
+// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
-});
-
+  through: {
+    model: 'ProductTag',
+    foreignKey: 'product_id',
+  },
+}),
 module.exports = {
   Product,
   Category,
