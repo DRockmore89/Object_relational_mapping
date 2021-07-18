fe2bc87410dbdee3263e85990392294940f79ce7
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-18 17:23:03 -0400

    commit

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag } = require('../../routes/api');
 
 // The `/api/tags` endpoint
 
